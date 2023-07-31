c85bc4a95e9afd637bad37e488314c7574ba34c3
Darshan Jagani <darshanjagani1902@gmail.com>
2023-07-31 13:52:52 +0530

    styled components and used the MaterialUi

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import './App.css';
-import { Route, Routes,NavLink } from 'react-router-dom';
+import { Route, Routes, NavLink } from 'react-router-dom';
 import Welcome from './components/Welcome';
 import User from './components/User';
-
-
+import { Button } from '@material-ui/core';
 
 function App() {
   return (
     <div>
-      <div>
-      <NavLink to='/'> HOME </NavLink><br/>
-      <NavLink to='/welcome'> WELCOME </NavLink><br/>
-      <NavLink to='/user'> USER </NavLink>
+      <div style={
+        {
+          display: 'flex',
+          justifyContent: 'space-around',
+          alignItems: 'center',
+          background: 'linear-gradient(120deg,pink,blueviolet)',
+          height: 70
+        }
+      }>
+
+        <Button variant="outlined" color="primary">
+          <NavLink to='/'> HOME </NavLink><br />
+        </Button>
+        <Button variant="outlined" color="primary">
+          <NavLink to='/welcome'> WELCOME </NavLink><br />
+        </Button>
+        <Button variant="outlined" color="primary">
+          <NavLink to='/user'> USER </NavLink>
+        </Button>
       </div>
+    
       <Routes path='/'>
         <Route index element={<h1>this is HOME PAGE</h1>} />
-        <Route path='/welcome' element={ <Welcome/>} />
-        <Route path='/user' element={<User name="darshan" role="student"/>}/>
+        <Route path='/welcome' element={<Welcome />} />
+        <Route path='/user' element={<User name="darshan" role="student" />} />
       </Routes>
     </div>
   );
--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import SimpleCard from './Card';
+
 
 export default function User(props) {
   return (
-    <div>
+   <>
+     <div>
         <h1>{props.name}</h1>
         <h2>{props.role}</h2>
     </div>
+    <SimpleCard/>
+
+   </>
+
   )
 }
